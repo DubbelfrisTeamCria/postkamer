20edd7680c698a0a2908cb98cc73159a5b3b20d8
amber Schühmacher <amberebel@gmail.com>
2013-05-27 14:11:44 +0200

    added canvas in editor

--- app/controllers/homepageController.js
+++ app/controllers/homepageController.js
@@ -2,8 +2,8 @@
 //The $scope is ultimately bound to the customers view
 app.controller('HomepageCtrl', function( myService,$scope) {
     // Call the async method and then do stuff with what is returned inside our own then function
-    myService.async().then(function(d) {
-        $scope.test = d;
+    myService.async().then(function(data) {
+        $scope.test = data;
 
 
     });
--- app/controllers/templateController.js
+++ app/controllers/templateController.js
@@ -1,6 +1,6 @@
 app.controller('TemplateCtrl', function( myService,$scope) {
     // Call the async method and then do stuff with what is returned inside our own then function
-    myService.async().then(function(d) {
-        $scope.customers = d;
+    myService.async().then(function(data) {
+        $scope.customers = data;
     });
 });
