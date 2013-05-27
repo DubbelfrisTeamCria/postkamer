ee168070bfc1cb01dead8aaf588e3e2ed799a400
amber Schühmacher <amberebel@gmail.com>
2013-05-27 15:36:39 +0200

    addImage via url function added

--- app/controllers/editorController.js
+++ app/controllers/editorController.js
@@ -4,3 +4,24 @@ app.controller('EditorCtrl', function( myService,$scope) {
         $scope.customers = data;
     });
 });
+
+function askURL() {
+    var url = prompt("Please give the url of the image");
+    addImage(url);
+}
+
+function addImage(src) {
+    canvas = document.getElementById('canvas')
+    context = canvas.getContext('2d');
+    var image = new Image();
+    image.src = src;
+    image.onload = function(){
+        context.drawImage(image, 2, 2);
+    }
+}
+
+
+
+
+
+
