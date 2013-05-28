ac69c41c0b57cf41cbd961a073a68c91d113abf7
amber Schühmacher <amberebel@gmail.com>
2013-05-28 13:45:44 +0200

    fixed mistake

--- app/controllers/editorController.js
+++ app/controllers/editorController.js
@@ -1,6 +1,6 @@
 app.controller('EditorCtrl', function( service,$scope) {
     // Call the async method and then do stuff with what is returned inside our own then function
-    srvice.async().then(function(data) {
+    service.async().then(function(data) {
         $scope.customers = data;
     });
 });
