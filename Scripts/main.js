cf0e63cc4f0076d9311d1759e256d7ec11fd6289
amber Schühmacher <amberebel@gmail.com>
2013-05-28 14:12:28 +0200

    canvas changes

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,18 +1,23 @@
-$(function(){
-    function askURL() {
-        var url = prompt("Please give the url of the image");
-        addImage(url);
-    }
+window.onLoad=function(){
+    canvas = $('#canvas');
+    context = canvas.getContext('2d');
+    canvas.css({backgroundColor:"rgb(245, 8, 8)"});  //werkt nog niet
+};
+var canvas, context;
 
-    function addImage(src) {
-        canvas = document.getElementById('canvas');
-        context = canvas.getContext('2d');
-        var image = new Image();
-        image.src = src;
-        image.onload = function(){
-            context.drawImage(image, 2, 2);
-        }
+function askURL() {
+    var url = prompt("Please give the url of the image");
+    addImage(url);
+}
+
+function addImage(src) {
+
+    var image = new Image();
+    image.src = src;
+    image.onload = function(){
+        context.drawImage(image, 2, 2);
     }
-});
+}
+
 
 
