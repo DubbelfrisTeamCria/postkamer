80682ae943960acdb6fef4409ad966c908377c27
Almira Jahja <A.Jahja@student.han.nl>
2013-05-29 12:54:06 +0200

    canvas text

---
+++ app/models/Text.js
@@ -0,0 +1,22 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: Almira
+ * Date: 29-5-13
+ * Time: 12:32
+ * To change this template use File | Settings | File Templates.
+ */
+
+function Text(){
+
+    this.text = null;
+
+    this.kleur = null;
+
+    this.grootte = null;
+
+    this.positieY = null;
+
+    this.positieX = null;
+
+    this.textStyle = null;
+}
