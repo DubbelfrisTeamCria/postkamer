1038154a0bb8b4410d2b1f5f1b565ae12e41ffa2
amber Schühmacher <amberebel@gmail.com>
2013-05-29 11:58:25 +0200

    canvas achtergrondkleur grootte en klaar voor colorpicker

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,24 +1,20 @@
-window.onLoad=function(){
-    canvas = $('#canvas');
-    context = canvas.getContext('2d');
-    canvas.css({backgroundColor:"rgb(245, 8, 8)"});  //werkt nog niet
-};
-var canvas, context;
-
 function askURL() {
     var url = prompt("Please give the url of the image");
     addImage(url);
 }
 
 function vulKleur(){
-   var c = document.getElementById('canvas');
-    var ctx = c.getContext('2d');
-    ctx.rect(188, 50, 200, 100);
-    ctx.fillStyle="red";
-    console.log(ctx.fillStyle);
-    console.log(ctx.fill);
+    canvas = document.getElementById('canvas');
+    context = canvas.getContext('2d');
+    context.rect(0, 0, canvas.width, canvas.height);
+    var achtergrondkleur = "006DCC";                //kleur uit colorpicker halen wanneer deze bestaat.
+    context.fillStyle = achtergrondkleur;
+    context.fill();
 }
+
 function addImage(src) {
+    canvas = document.getElementById('canvas');
+    context = canvas.getContext('2d');
     var image = new Image();
     image.src = src;
     image.onload = function(){
