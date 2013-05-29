875c74d0820ff0e63d767f915bc49832b4c0a5c9
amber Schühmacher <amberebel@gmail.com>
2013-05-29 15:07:53 +0200

    OntwerpObject aangepast

--- app/models/OntwerpObject.js
+++ app/models/OntwerpObject.js
@@ -6,42 +6,50 @@ function OntwerpObject(){
     this._transparantie = null;
 }
 
-OntwerpObject.prototype.getHoogte = function () {
+OntwerpObject.prototype.getHoogte = function() {
     return this._hoogte;
 };
 
-OntwerpObject.prototype.setHoogte = function (hoogte) {
+OntwerpObject.prototype.setHoogte = function(hoogte) {
     this._hoogte = hoogte;
 };
 
-OntwerpObject.prototype.getBreedte = function () {
+OntwerpObject.prototype.getBreedte = function() {
     return this._breedte;
 };
 
-OntwerpObject.prototype.setBreedte = function (breedte) {
+OntwerpObject.prototype.setBreedte = function(breedte) {
     this._breedte = breedte;
 };
 
-OntwerpObject.prototype.getPositieY = function () {
+OntwerpObject.prototype.getPositieY = function() {
     return this._positieY;
 };
 
-OntwerpObject.prototype.setPositieY = function (positieY) {
+OntwerpObject.prototype.setPositieY = function(positieY) {
     this._positieY = positieY;
 };
 
-OntwerpObject.prototype.getPositieX = function () {
+OntwerpObject.prototype.getPositieX = function() {
     return this._positieX;
 };
 
-OntwerpObject.prototype.setPositieX = function (positieX) {
+OntwerpObject.prototype.setPositieX = function(positieX) {
     this._positieX = positieX;
 };
 
-OntwerpObject.prototype.getTransparantie = function () {
+OntwerpObject.prototype.getTransparantie = function() {
     return this._transparantie;
 };
 
 OntwerpObject.prototype.setTransparantie = function (transparantie) {
     this._transparantie = transparantie;
 };
+
+OntwerpObject.prototype.resize = function() {
+//resizen van OntwerpObject
+}
+
+OntwerpObject.prototype.verwijder = function() {
+//Verwijderen van OntwerpObject
+}
