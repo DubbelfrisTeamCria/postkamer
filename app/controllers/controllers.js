bb82e1798edb2653db73064d5423494facb7e38a
Almira Jahja <A.Jahja@student.han.nl>
2013-05-30 13:21:13 +0200

    postkamer

--- app/controllers/controllers.js
+++ app/controllers/controllers.js
@@ -1,4 +1,4 @@
-﻿/*
+﻿/*la
 app.controller('CustomersController', function ($scope, customersService) {
 
     //I like to have an init() for controllers that need to perform some initialization. Keeps things in
