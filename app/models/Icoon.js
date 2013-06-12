fcf797b4d9d6c658278985d2705ba8a0b8ff285d
Almira Jahja <A.Jahja@student.han.nl>
2013-06-12 12:55:36 +0200

    shadowaart en models als commentaar

--- app/models/Icoon.js
+++ app/models/Icoon.js
@@ -1,14 +1,14 @@
-
-function Icoon() {
-    this._naam = null;
-}
-
-Icoon.prototype = new OntwerpObject;
-
-Icoon.prototype.getNaam = function() {
-    return this._naam;
-}
-
-Icoon.prototype.setNaam = function(naam) {
-    this._naam = naam;
-}
+//
+//function Icoon() {
+//    this._naam = null;
+//}
+//
+//Icoon.prototype = new OntwerpObject;
+//
+//Icoon.prototype.getNaam = function() {
+//    return this._naam;
+//}
+//
+//Icoon.prototype.setNaam = function(naam) {
+//    this._naam = naam;
+//}
