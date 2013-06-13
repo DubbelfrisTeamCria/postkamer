403c8bddd0c70c40921dc534f5f4207dce494298
juan David Castellanos <juandavid.castellanos@live.com>
2013-06-13 15:13:14 +0200

    tools opzet

--- app/controllers/overzichtController.js
+++
@@ -1,6 +0,0 @@
-app.controller('OverzichtCtrl', function( service,$scope) {
-    // Call the async method and then do stuff with what is returned inside our own then function
-    service.async().then(function(data) {
-        $scope.customers = data;
-    });
-});
---
+++ client/app/controllers/editorController.js
@@ -0,0 +1,92 @@
+app.controller('EditorCtrl', function( service,$scope) {
+    // Call the async method and then do stuff with what is returned inside our own then function
+    service.async().then(function(data) {
+        $scope.customers = data;
+       // console.log(data[26]);
+
+    });
+
+    editor()
+
+//    var voorkantCanvas = new editor('canvas');
+//   var binnenkantCanvas = new editor('binnenkantcanvas');
+//   // var envelopCanvas  = new editor('envelopcanvas');
+
+
+     var voorkant = $("#canvas");
+    var binnenkant = $("#binnenkantcanvas");
+    var envelop = $("#envelopcanvas");
+
+
+
+
+
+
+
+
+//    function setHidden(){
+//        binnenkant.parent().css('display' ,'none');
+//        envelop.parent().css('display' ,'none');
+//
+//    }
+//
+//    setHidden();
+//
+//
+//
+//    $('#voorKant').click(function(){
+//        voorkant.parent().css('display' ,'block');
+//        binnenkant.parent().css('display' ,'none');
+//        envelop.parent().css('display' ,'none');
+//    });
+//
+//
+//
+//    $('#binnenKant').click(function(){
+//        canvas = binnenkantcanvas;
+//        binnenkant.parent().css('display' ,'block');
+//        voorkant.parent().css('display' ,'none');
+//        envelop.parent().css('display' ,'none');
+//    });
+//
+//
+//    $('#envelop').click(function(){
+//        canvas = envelopcanvas;
+//        envelop.parent().css('display' ,'block');
+//        binnenkant.parent().css('display' ,'none');
+//        voorkant.parent().css('display' ,'none');
+//    });
+
+
+
+   $scope.save = function(){
+
+        var data = getJSON();
+        console.log(data);
+
+        service.saveTemplate(data);
+    }
+
+
+
+    $scope.load = function(){
+     service.async().then(function(data) {
+
+           loadTemplate(data[3]);
+        })
+
+    }
+
+
+
+
+
+
+});
+
+
+
+
+
+
+
